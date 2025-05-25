3cbbe58df2e7f81a5fcc5a958f9256b51b891c6f
Official-Regify <official.regify@gmail.com>
2025-05-25 06:55:19 +0000

    Test update

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log(`Starting Regify Server...`);
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.get("/create", (req, res) => {
+  res.sendFile(path.join(__dirname, "create.html"))
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});

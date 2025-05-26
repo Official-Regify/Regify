9d518eab939e51f206642a415f7456e6465a91bc
Regify <official.regify@gmail.com>
2025-05-26 04:20:24 +0000

    Update INDEX. HTML  for better looks

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // we'll set this in Railway
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 // Serve the HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));

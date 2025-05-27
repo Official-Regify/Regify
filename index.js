17543cb25b7ba05b737ccc35502230ad4bdf72c8
Regify <official.regify@gmail.com>
2025-05-27 02:08:48 +0000

    Better index

--- index.js
+++ index.js
@@ -9,21 +9,45 @@ app.use(express.static('public'));
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // we'll set this in Railway
+  connectionString: process.env.POSTGRES_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+  },
+});
+
+module.exports = pool;
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('DB connected:', res.rows);
   }
 });
+
+pool.query(`
+  CREATE TABLE IF NOT EXISTS events (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    date TEXT,
+    description TEXT
+  );
+`);
+
 // Serve the HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-
+})
 app.get("/create", (req, res) => {
-  res.sendFile(path.join(__dirname, "create.html"))
+  res.sendFile(path.join(__dirname,"create.html"))
 });
 
+app.post("/events", (req, res) => {
+  const {id, name, date, description} = req.query;
+  
+  pool.query(`INSERT INTO events (id, title, date, description) VALUES ($1,$2,$3,$4)`,[id, name, date, description ])
+  
+});
 
 // Start the server
 app.listen(port,"0.0.0.0", () => {
